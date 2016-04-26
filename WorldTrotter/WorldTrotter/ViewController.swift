f11830a684f0194e3d9d5f5d3c2eb9a7cd11a7e4
Michael Owens <mowens@ucdavis.edu>
2016-04-26 00:21:40 -0700

    Ch.3 autolayour and views. Add constraints to labels

---
+++ WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Michael Owens on 4/24/16.
+//  Copyright © 2016 Coding a Dream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    
+}
+
